4ba77bfa59e3e511cb3359c2bde40fe9dc0a8ca2
Wasim6205 <wasimbhai6205@gmail.com>
2024-11-05 15:10:43 +0530

    update some functionality

--- backend/controllers/checkout.js
+++ backend/controllers/checkout.js
@@ -3,4 +3,22 @@ import crypto from 'crypto';
 import { Volunteer } from "../models/volunteer.js";
 
 // BASE URL
-const cryptomus = axios.create({baseURL: ""})
+const cryptomus = axios.create({ baseURL: "https://api.cryptomus.com/v1" });
+
+const createInvoice = async (amount) => {
+    try {
+        const data = {
+            amount: amount,
+            currency: "USD",
+            order_id: crypto.randomBytes(12).toString("hex"),
+            url_return: "https://donation-website-orcin.vercel.app/donate",
+            url_success: "https://donation-website-orcin.vercel.app",
+            lifetime: 300,
+        };
+
+        const sign = crypto.createHash("md5").update(Buffer.from(JSON.stringify(data)).toString("base64") + process.env.PAYMENT_API_KEY)
+        
+    } catch (error) {
+        
+    }
+}
--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <section className="notfound">
+      <div className="content">
+        <h1>404 Not Found</h1>
+        <Link to={"/"} className="btn">Back to home page</Link>
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
